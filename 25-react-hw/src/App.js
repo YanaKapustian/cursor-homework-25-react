8d9f41b722580c7b806f3c8f883ef3c78731303d
Yana Kapustian <yana2015.2004@gmail.com>
2022-08-03 18:50:47 +0300

    hw 25

--- 25-react-hw/src/App.js
+++ 25-react-hw/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React from 'react';
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import {HashRouter, Routes, Route, Navigate} from "react-router-dom";
 import Homepage from './components/Homepage';
 import Navbar from './components/Navbar';
 import Twitter from './components/Twitter';
@@ -10,16 +10,17 @@ import ContactsPages from './components/ContactsPages';
 
 function App() {
   return (
-    <Router>
+    <HashRouter basename='/'>
       <Navbar />
           <Routes>
             <Route exact path='/homepage' element={<Homepage />}></Route>
+            <Route exact path="/" element={ <Navigate to="/homepage" /> }></Route>
             <Route exact path='/twitter' element={<Twitter />}></Route>
             <Route exact path='/instagram' element={<Instagram />}></Route>
             <Route exact path='/contacts' element={<Contacts />}></Route>
             <Route exact path='/contacts/:username' element={<ContactsPages />}></Route>
           </Routes>
-    </Router>
+    </HashRouter>
   );
 }
 
