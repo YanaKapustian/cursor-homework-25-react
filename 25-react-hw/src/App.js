05999a27d94b0055cfd29959d0c1bcb38c62a435
Yana Kapustian <yana2015.2004@gmail.com>
2022-08-03 11:34:28 +0300

    hw25

--- 25-react-hw/src/App.js
+++ 25-react-hw/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React from 'react';
-import {BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Homepage from './components/Homepage';
 import Navbar from './components/Navbar';
 import Twitter from './components/Twitter';
@@ -13,9 +13,6 @@ function App() {
     <Router>
       <Navbar />
           <Routes>
-            <Route exact path="/cursor-homework-25-react">
-               <Navigate to="/homepage"/>
-            </Route>
             <Route exact path='/homepage' element={<Homepage />}></Route>
             <Route exact path='/twitter' element={<Twitter />}></Route>
             <Route exact path='/instagram' element={<Instagram />}></Route>
